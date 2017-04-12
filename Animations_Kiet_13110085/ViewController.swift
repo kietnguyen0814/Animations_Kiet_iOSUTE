9c0a90f24e8d0604c9b9eaf49def087564216ec6
Cntt01 <Admin01@Cntt01s-iMac.local>
2017-04-12 14:46:34 +0700

    Initial Commit

---
+++ Animations_Kiet_13110085/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animations_Kiet_13110085
+//
+//  Created by Cntt01 on 4/12/17.
+//  Copyright © 2017 Group9_UTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
